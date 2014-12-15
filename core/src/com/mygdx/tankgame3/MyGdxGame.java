ba5d69f9e43f1badd06ff72a7b822468b61ca918
Noah <Noah@Noah>
2014-12-14 21:19:58 -0500

    testing

--- core/src/com/mygdx/tankgame3/MyGdxGame.java
+++ core/src/com/mygdx/tankgame3/MyGdxGame.java
@@ -12,6 +12,7 @@ public class MyGdxGame extends ApplicationAdapter {
 	
 	@Override
 	public void create () {
+            //test
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
